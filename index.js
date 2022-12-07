c92126284d58626723cd098824747814354c21ae
MTaningco <jmigst@gmail.com>
2022-12-07 12:11:14 -0800

    Create GET request endpoint.

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/latestMatches", async (req, res) => {
+    const summonerName = req.query.summoner;
+    if (summonerName) {
+        res.json(summonerName);
+    } else {
+        res.json("forbidden");
+    }
+});
+
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
 });
